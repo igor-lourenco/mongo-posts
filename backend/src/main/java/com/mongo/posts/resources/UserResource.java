05e8adbf4ed5ee596dc359e3b83365f685325eb0
igor-lourenco <igor.lourencosantos@gmail.com>
2021-09-24 22:12:17 -0300

    GET User by id

--- backend/src/main/java/com/mongo/posts/resources/UserResource.java
+++ backend/src/main/java/com/mongo/posts/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,12 @@ public class UserResource {
 	public ResponseEntity<List<UserDTO>> findAll(){
 		List<UserDTO> entity = service.findAll();
 		return ResponseEntity.ok().body(entity);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		UserDTO entity = service.findById(id);
+		return ResponseEntity.ok().body(entity);
+				
 	}
 }
--- backend/src/main/java/com/mongo/posts/services/UserService.java
+++ backend/src/main/java/com/mongo/posts/services/UserService.java
@@ -1,6 +1,7 @@
 package com.mongo.posts.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.mongo.posts.models.dto.UserDTO;
 import com.mongo.posts.models.entities.User;
 import com.mongo.posts.repositories.UserRepository;
+import com.mongo.posts.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,11 @@ public class UserService {
 	public List<UserDTO> findAll(){
 		List<User> entity = repository.findAll();
 		return entity.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	public UserDTO findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado -> " + id));
+		return new UserDTO(entity);
 	}
 }
