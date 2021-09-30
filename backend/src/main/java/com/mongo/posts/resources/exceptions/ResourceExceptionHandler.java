038a5e31d30a08481a0a88eb385a7ccd766cf93f
igor-lourenco <igor.lourencosantos@gmail.com>
2021-09-30 20:01:38 -0300

    Exception Handling

---
+++ backend/src/main/java/com/mongo/posts/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.mongo.posts.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.mongo.posts.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError error = new StandardError();
+		error.setError("Not found");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		error.setStatus(status.value());
+		error.setTimestamp(Instant.now());
+	
+		return ResponseEntity.status(status).body(error);
+	}
+}
+
